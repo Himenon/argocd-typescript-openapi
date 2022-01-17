62998631a30259c1f0ac66cb51caa20998e18aa5
kousei-himeno <kousei_himeno@dwango.co.jp>
2022-01-17 11:25:15 +0900

    feat: initialize

---
+++ scripts/build.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import rimraf from "rimraf";
+import { shell } from "./tools/shell";
+import { copyPackageSet } from "./tools/copyPackageSet";
+
+const main = async () => {
+  rimraf.sync("lib");
+  await Promise.all([
+    shell("yarn tsc -p tsconfig.esm.json -d --emitDeclarationOnly --outDir ./lib/\\$types"),
+    shell("yarn tsc -p tsconfig.cjs.json"),
+    shell("yarn tsc -p tsconfig.esm.json"),
+  ]);
+  await shell("cherry-pick --cwd ./lib --input-dir ../src --types-dir ./\\$types --cjs-dir ./\\$cjs --esm-dir ./\\$esm");
+  await copyPackageSet();
+};
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
---
+++ scripts/config.ts
@@ -0,0 +1,6 @@
+/**
+ * @see https://github.com/argoproj/argo-cd/blob/master/assets/swagger.json
+ */
+export const versions = ["v2.2.2", "v2.1.8", "v2.1.6", "v1.8.7"];
+export const outputDir = "api-spec";
+export const sourceOutputDir = "src";
