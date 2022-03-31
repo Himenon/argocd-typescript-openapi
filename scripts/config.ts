a0300b223cd3e2f42be26ee007b82caf7c11a719
K.Himeno <k.himeno314@gmail.com>
2022-03-31 18:51:29 +0900

    feat: add argo-cd@v2.3.3 (#3)

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,6 @@
 /**
  * @see https://github.com/argoproj/argo-cd/blob/master/assets/swagger.json
  */
-export const versions = ["v2.2.2", "v2.1.8", "v2.1.6", "v1.8.7"];
+export const versions = ["v2.3.3", "v2.2.2", "v2.1.8", "v2.1.6", "v1.8.7"];
 export const outputDir = "api-spec";
 export const sourceOutputDir = "src";
